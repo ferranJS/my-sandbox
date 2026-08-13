d1f9d1cd396f2da8e1d6b732cd4f1c7fa8e24a40
ferranJS <ferranramirezmarti@gmail.com>
2026-08-13 20:09:45 +0200

    grid of apps instead of buttons

---
+++ ovdr/circles.js
@@ -0,0 +1,24 @@
+
+
+function startCircle() {
+    [x0, y0] = [x, y]
+}
+
+function previewCircle(e)  {    
+    drawPaths()
+    ctx_tmp.beginPath()
+    getPosition(e)
+    ctx_tmp.arc(...midpoint(x0,y0,x,y), radius(x0,y0,x,y), 0, 2*Math.PI)
+    changeGrosor(4)
+    ctx_tmp.stroke()
+    changeGrosor()
+}
+
+
+function midpoint(x1, y1, x2, y2) {
+	return [(x1 + x2)/2, (y1 + y2)/2]
+}
+
+function radius(x1, y1, x2, y2) {
+    return Math.hypot(x2 - x1, y2 - y1)/2
+  }
